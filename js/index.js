ef66231030481afd154669b3252bf0d60fedaaf3
9161-lebedevalr <lubov.lebedeva@mail.ru>
2016-06-29 22:38:41 +0400

    4 commit

--- js/index.js
+++ js/index.js
@@ -97,3 +97,16 @@ submit.addEventListener('click', function(e) {
     } // end loop
     if ( stopSubmit ) { e.preventDefault(); }
 });
+
+CustomValidation.prototype.checkValidity = function(input) {
+
+    // In-built validity checks here ------
+
+    // Custom Validity checks ------
+    if ( !input.value.match(/[a-z]/g) ) {
+        this.addInvalidity('At least 1 lowercase letter is required');
+    }
+    if ( !input.value.match(/[A-Z]/g) ) {
+    this.addInvalidity('At least 1 uppercase letter is required');
+    }
+}
